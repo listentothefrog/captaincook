f4ff5c5e1c10c4283968520ce8fc23350615eedd
Shashank Ellareddy <66846202+listentothefrog@users.noreply.github.com>
2021-07-16 19:54:41 -0400

    custom tailwind properties

--- triumph/tailwind.config.js
+++ triumph/tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Montserrat"]
+      }, 
+      colors: {
+        'tan': '#e0cbaf',
+        'red': '#FF6347',
+        'hover-red': '#FF6145',
+        'disabled-red': '#D44C33',
+      }, 
+    },
+    fontWeight: {
+      normal: 400, 
+      medium: 500, 
+      semibold: 600, 
+      bold: 700, 
+      extrabold: 800, 
+    }, 
   },
   variants: {
     extend: {},
