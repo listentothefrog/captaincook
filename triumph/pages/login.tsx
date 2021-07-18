a95f5b027d3dab64e9ee8b9af6462cadc9684e2a
listentothefrog <shashanke7y@gmail.com>
2021-07-18 13:44:56 -0400

    changed sign in to login

--- triumph/pages/login.tsx
+++ triumph/pages/login.tsx
@@ -58,7 +58,7 @@ const login = () => {
             type="submit"
             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red hover:bg-hover-red focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Sign in
+            Login
           </button>
         </div>
       </div>
