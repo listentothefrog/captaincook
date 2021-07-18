97163e8744631aa2ab8c3c208eeb148883cd9ad5
listentothefrog <shashanke7y@gmail.com>
2021-07-18 16:16:47 -0400

    Removed link from Logo

--- triumph/components/Header.tsx
+++ triumph/components/Header.tsx
@@ -6,9 +6,7 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between w-screen h-16  bg-tan">
       <div className="mt-2 ml-4">
-        <NextLink href="/">
-          <Image src={logo} className="cursor-pointer" />
-        </NextLink>
+        <Image src={logo} className="cursor-pointer" />
       </div>
       <div className="flex items-center justify-end w-92">
         <div className="flex w-full items-center">
