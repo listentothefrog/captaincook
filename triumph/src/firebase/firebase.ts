3cda9e561b86d220da6b492156489db48809b80f
listentothefrog <shashanke7y@gmail.com>
2021-07-23 14:14:45 -0400

    init firebase features

---
+++ triumph/src/firebase/firebase.ts
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/analytics";
+import { firebaseConfig } from "./firebaseConfig";
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+firebase.analytics();
+
+export default { auth };
