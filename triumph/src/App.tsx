753b6c8c8744df21007a59f6ac9b3c0706b5fe29
listentothefrog <shashanke7y@gmail.com>
2021-07-27 11:15:50 -0400

    returns what route the user is on

--- triumph/src/App.tsx
+++ triumph/src/App.tsx
@@ -1,9 +1,58 @@
-import Header from "./components/Header";
-const App = () => {
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  RouteComponentProps,
+} from "react-router-dom";
+import { auth } from "./firebase/firebase";
+import routes from "./firebase/route";
+import AuthRoute from "./modules/auth/AuthRoute";
+
+export interface IApplicationProps {}
+
+const App: React.FC<IApplicationProps> = (props) => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // Monitor and Update user state.
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log("User detected.");
+      } else {
+        console.log("No user detected");
+      }
+      setLoading(false);
+    });
+  }, []);
+  if (loading) return <div>Loding...</div>;
+
   return (
-    <div>
-      <Header />
-    </div>
+    <Router>
+      <Switch>
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            render={(routeProps: RouteComponentProps<any>) => {
+              if (route.protected)
+                return (
+                  <AuthRoute>
+                    <route.component {...routeProps} />
+                  </AuthRoute>
+                );
+
+              return (
+                <div>
+                  <route.component {...routeProps} />
+                </div>
+              );
+            }}
+          />
+        ))}
+      </Switch>
+    </Router>
   );
 };
 
