9a3f3d458b69b7122264dba14866ba797877aa5a
Shashank Ellareddy <66846202+listentothefrog@users.noreply.github.com>
2021-07-29 21:46:31 -0400

    Base Chakra ui Header

--- triumph/src/components/Header.tsx
+++ triumph/src/components/Header.tsx
@@ -1,9 +1,44 @@
-const Header = () => {
+import { Box, Flex } from "@chakra-ui/react";
+import logo from "../images/logo-web.svg";
+
+const HeaderComponent = () => {
   return (
-    <div>
-      <h1 className="font-bold">Hello!</h1>
-    </div>
+    <Flex
+      justifyContent={"space-between"}
+      background={"tan"}
+      alignContent={"center"}
+    >
+      <Box marginTop={"2"} marginLeft={"4"}>
+        <img src={logo} className="cursor-pointer" alt="logo" />
+      </Box>
+      <Box className="flex items-center justify-end w-92 mr-10">
+        <Flex
+          margin={"2"}
+          width={"full"}
+          alignItems="center"
+          className="flex w-full items-center"
+        >
+          <Box
+            marginRight={"2"}
+            cursor={"pointer"}
+            fontWeight={"medium"}
+            className="mr-10 cursor-pointer hover:underline mt-2 font-normal"
+            fontFamily={"body"}
+          >
+            Login
+          </Box>
+          <Box
+            margin={"2"}
+            fontWeight={"medium"}
+            fontFamily={"body"}
+            className="mr-10 cursor-pointer hover:underline mt-2 font-normal"
+          >
+            Register
+          </Box>
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
 
-export default Header;
+export default HeaderComponent;
