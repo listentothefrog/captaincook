63ee0eb72850fb34f7921fa240e3bc99dc48b976
listentothefrog <shashanke7y@gmail.com>
2021-07-30 12:33:37 -0400

    "link" the fonts

--- triumph/src/index.tsx
+++ triumph/src/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "@fontsource/poppins/500.css";
+import "@fontsource/montserrat/700.css";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+// custom fonts
+const theme = extendTheme({
+  fonts: {
+    heading: "Montserrat",
+    body: "Poppins",
+  },
+});
+
+export default theme;
+
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
