a4bd0ca4fcb84f01a07e9d202e5777645ba43207
listentothefrog <shashanke7y@gmail.com>
2021-07-29 20:06:07 -0400

    Removed Link wrapper

--- triumph/src/components/Header.tsx
+++ triumph/src/components/Header.tsx
@@ -1,27 +1,9 @@
-import logo from "../images/logo-web.svg";
-import { Link } from "react-router-dom";
-// import IpageProps from "src/interfaces/page.interface";
-
-const HeaderComponent = () => {
+const Header = () => {
   return (
-    <div className="font-body flex items-center justify-between w-screen h-16  bg-tan">
-      <div className="mt-2 ml-4">
-        <img src={logo} className="cursor-pointer" alt="logo" />
-      </div>
-      <div className="flex items-center justify-end w-92 mr-10">
-        <div className="flex w-full items-center">
-          <Link to={`/login`}>
-            <div className="mr-10 cursor-pointer hover:underline mt-2 font-normal">
-              Login
-            </div>
-          </Link>
-          <div className="mr-10 cursor-pointer hover:underline mt-2 font-normal">
-            Register
-          </div>
-        </div>
-      </div>
+    <div>
+      <h1 className="font-bold">Hello!</h1>
     </div>
   );
 };
 
-export default HeaderComponent;
+export default Header;
