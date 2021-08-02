b9fc5d097146eea7cf0a16ec25ab2395983843ac
listentothefrog <shashanke7y@gmail.com>
2021-08-02 16:43:12 -0400

    routes

---
+++ triumph/src/firebase/routes.ts
@@ -0,0 +1,22 @@
+import IRoute from "src/interfaces/route";
+import register from "src/pages/auth/register";
+import home from "src/pages/home";
+
+const routes: IRoute[] = [
+  {
+    path: "/",
+    exact: true,
+    component: home,
+    name: "Home Page",
+    protected: true,
+  },
+  {
+    path: "/register",
+    exact: true,
+    component: register,
+    name: "Register Page",
+    protected: false,
+  },
+];
+
+export default routes;
