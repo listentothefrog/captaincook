d0bed64dfbb42ae89d8207807ae7063d5f80b162
listentothefrog <shashanke7y@gmail.com>
2021-08-02 21:00:36 -0400

    register page that renders with Register Component

--- triumph/src/pages/auth/register.tsx
+++ triumph/src/pages/auth/register.tsx
@@ -1,55 +1,7 @@
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-import { auth } from "src/firebase/firebase";
+import RegisterComponent from "src/components/Register";
 import IPageProps from "src/interfaces/page";
 
 const register: React.FC<IPageProps> = (props) => {
-  const [registering, setRegistering] = useState<boolean>(false);
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [confirmPassoword, setConfirmPassoword] = useState<string>("");
-  const [error, setError] = useState<string>("");
-  const history = useHistory();
-
-  const createUserWithEmailandPassword = async () => {
-    if (password !== confirmPassoword)
-      toast.error("password's don't match", {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-
-    if (error !== "") setError("");
-    setRegistering(true);
-    await auth
-      .createUserWithEmailAndPassword(email, password)
-      .then((result) => {
-        toast.info(
-          "you have succefully created an account, a email verification link was sent to you inbox",
-          {
-            position: "top-right",
-            autoClose: 4000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-          }
-        );
-        history.push("/login");
-      })
-      .catch((err) => {});
-  };
-  return (
-    <div>
-      <ToastContainer />
-    </div>
-  );
+  return <RegisterComponent />;
 };
-
 export default register;
