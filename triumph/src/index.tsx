5b00d7652345c51c06b2487ba18179dd20979f03
listentothefrog <shashanke7y@gmail.com>
2021-08-02 21:00:07 -0400

    change font weight for poppins

--- triumph/src/index.tsx
+++ triumph/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/400.css";
 import "@fontsource/montserrat/700.css";
 import App from "./App";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
