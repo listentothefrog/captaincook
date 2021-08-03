5d770dcaab31f0439ddce18999b2fef09f5d597e
listentothefrog <shashanke7y@gmail.com>
2021-08-03 11:36:45 -0400

    redirects users to login if they not logged in

--- triumph/src/App.tsx
+++ triumph/src/App.tsx
@@ -1,11 +1,60 @@
-import HeaderComponent from "./components/Header";
-import RegisterComponent from "./components/Register";
+import React, { useEffect, useState } from "react";
+import { Route, RouteComponentProps, Switch } from "react-router-dom";
+import { auth } from "./firebase/firebase";
+import { Spinner } from "@chakra-ui/react";
+import routes from "./firebase/routes";
+import AuthRoute from "./components/AuthRoute";
 
-const App = () => {
+export interface IApplicationProps {}
+const App: React.FC<IApplicationProps> = (props) => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.info("User detected.");
+      } else {
+        console.info("No user detected");
+      }
+
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading)
+    return (
+      <Spinner
+        thickness="3px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="#6c9e4f"
+        size="md"
+      />
+    );
+
   return (
     <div>
-      <HeaderComponent />
-      <RegisterComponent />
+      <div>
+        <Switch>
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              render={(routeProps: RouteComponentProps<any>) => {
+                if (route.protected)
+                  return (
+                    <AuthRoute>
+                      <route.component {...routeProps} />
+                    </AuthRoute>
+                  );
+
+                return <route.component {...routeProps} />;
+              }}
+            />
+          ))}
+        </Switch>
+      </div>
     </div>
   );
 };
