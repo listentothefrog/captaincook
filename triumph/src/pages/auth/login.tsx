e126bec921486ae973afc9cab32fda855f602e99
listentothefrog <shashanke7y@gmail.com>
2021-08-02 21:59:11 -0400

    login route that renders login component

---
+++ triumph/src/pages/auth/login.tsx
@@ -0,0 +1,8 @@
+import LoginComponent from "src/components/Login";
+import IPageProps from "src/interfaces/page";
+
+const login: React.FC<IPageProps> = (props) => {
+  return <LoginComponent />;
+};
+
+export default login;
