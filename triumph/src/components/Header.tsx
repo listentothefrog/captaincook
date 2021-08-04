579c8869a6bfe19c903fbe7b1e8c587cd8dabb09
listentothefrog <shashanke7y@gmail.com>
2021-08-04 11:25:24 -0400

    Displays user's email if they are logged in

--- triumph/src/components/Header.tsx
+++ triumph/src/components/Header.tsx
@@ -1,8 +1,11 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import logo from "../images/logo-web.svg";
+import { auth } from "src/firebase/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const HeaderComponent = () => {
+  const [user] = useAuthState(auth);
   return (
     <Flex
       justifyContent={"space-evenly"}
@@ -17,21 +20,15 @@ const HeaderComponent = () => {
       </Box>
       <Box>
         <Flex margin={"2"} width={"full"} alignItems="center">
-          <Box
-            marginRight={"2"}
-            cursor={"pointer"}
-            fontWeight={"medium"}
-            fontFamily={"body"}
-          >
-            <Link to="/login">Login</Link>
+          <Box marginRight={"2"} fontWeight={"medium"} fontFamily={"body"}>
+            {user ? "" : <Link to="/login">Login</Link>}
           </Box>
-          <Box
-            margin={"2"}
-            cursor={"pointer"}
-            fontWeight={"medium"}
-            fontFamily={"body"}
-          >
-            <Link to="/register">Register</Link>
+          <Box margin={"2"} fontWeight={"medium"} fontFamily={"body"}>
+            {user ? (
+              auth.currentUser?.email
+            ) : (
+              <Link to="/register">Register</Link>
+            )}
           </Box>
         </Flex>
       </Box>
