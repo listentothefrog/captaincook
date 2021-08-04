e572ae62698cf34a083c66badec52d91c032ae9a
listentothefrog <shashanke7y@gmail.com>
2021-08-04 15:53:58 -0400

    props for the pricing component

---
+++ triumph/src/interfaces/pricing.ts
@@ -0,0 +1,11 @@
+export default interface IPricingProps {
+  plan_title: string;
+  plan_price: number;
+  plan_image: any;
+  plan_perks_1: string;
+  plan_perks_2: string;
+  plan_perks_3: string;
+  plan_perks_4?: string;
+  plan_perks_5?: string;
+  plan_perks_6?: string;
+}
