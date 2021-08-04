9b3491a8b91b0a3b0657e844a42317166256b66e
listentothefrog <shashanke7y@gmail.com>
2021-08-03 21:26:06 -0400

    more elegant landing page

--- triumph/src/pages/home.tsx
+++ triumph/src/pages/home.tsx
@@ -9,10 +9,11 @@ const home = () => {
       <Flex
         justifyContent={"space-evenly"}
         width={"100%"}
-        mt={"4"}
-        height={"50vh"}
+        height={"100vh"}
         alignItems={"center"}
+        background={"#f5f5f5"}
         alignContent={"center"}
+        boxShadow="lg"
       >
         <Flex maxWidth={"50%"} maxHeight={"90vh"} flexDir={"column"}>
           <Heading>Cook like a pro chef.</Heading>
@@ -24,40 +25,41 @@ const home = () => {
             there will be a feed of recommended food where they can try out and
             add it to their "cook list".
           </Text>
+          <Flex
+            width={"100%"}
+            mt={"6"}
+            flexDirection={"row"}
+            alignItems={"center"}
+          >
+            <Box>
+              <Button
+                mr={"4"}
+                color={"white"}
+                background={"#6c9e4f"}
+                colorScheme={"whatsapp"}
+              >
+                <Link to="/register">Create an account</Link>
+              </Button>
+              <Button color={"white"} colorScheme={"purple"}>
+                Support CaptainCook
+              </Button>
+            </Box>
+          </Flex>
         </Flex>
-        <Flex height={"50vh"} alignContent={"center"} justifyContent={"center"}>
+        <Flex height={"80vh"} alignContent={"center"} justifyContent={"center"}>
           <Box>
             <Image
               src={
                 "https://images.unsplash.com/photo-1517244683847-7456b63c5969?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
               }
-              width={"300px"}
+              maxWidth={"100%"}
+              height={"100%"}
               alt={"Sweet"}
               boxShadow="lg"
               borderRadius={"lg"}
             />
           </Box>
         </Flex>
-      </Flex>
-      <Flex
-        justifyContent={"space-around"}
-        width={"47.5%"}
-        mt={"4"}
-        alignItems={"center"}
-      >
-        <Box>
-          <Button
-            mr={"4"}
-            color={"white"}
-            background={"#6c9e4f"}
-            colorScheme={"whatsapp"}
-          >
-            <Link to="/register">Create an account</Link>
-          </Button>
-          <Button color={"white"} colorScheme={"purple"}>
-            Support CaptainCook
-          </Button>
-        </Box>
       </Flex>
     </Box>
   );
