f3551b05859292e9192d37179883499837235785
listentothefrog <shashanke7y@gmail.com>
2021-08-09 10:32:32 -0400

    code split Search Component for better performance

--- triumph/src/pages/dashboard.tsx
+++ triumph/src/pages/dashboard.tsx
@@ -1,12 +1,27 @@
-import { Box } from "@chakra-ui/react";
-import SearchComponent from "src/components/DashBoard/Search";
+import { Box, Spinner } from "@chakra-ui/react";
+import React, { Suspense } from "react";
 import HeaderComponent from "src/components/Header";
+const SearchComponent = React.lazy(
+  () => import("src/components/DashBoard/Search")
+);
 
 const Dashboard = () => {
   return (
     <Box>
       <HeaderComponent />
-      <SearchComponent />
+      <Suspense
+        fallback={
+          <Spinner
+            thickness="3px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="#6c9e4f"
+            size="md"
+          />
+        }
+      >
+        <SearchComponent />
+      </Suspense>
     </Box>
   );
 };
