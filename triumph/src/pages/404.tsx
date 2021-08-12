fda62b4154386fa6fa049a09ec717400d9b9be03
listentothefrog <shashanke7y@gmail.com>
2021-08-12 10:49:58 -0400

    removed unused Header

--- triumph/src/pages/404.tsx
+++ triumph/src/pages/404.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
 import { linear } from "popmotion";
 import { Link } from "react-router-dom";
 import Lost from "../images/lost.svg";
