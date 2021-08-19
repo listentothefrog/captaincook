2672574b298d4a721b4da2eb7ae02804337fd8f5
listentothefrog <shashanke7y@gmail.com>
2021-08-19 16:12:15 -0400

    A better Dashboard Header Component

---
+++ triumph/src/components/DashBoard/DashboardHeader.tsx
@@ -0,0 +1,53 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+
+const DashboardHeader = () => {
+  return (
+    <Box mt={2}>
+      <Flex
+        width={"100%"}
+        height={"5vh"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        <Flex flexDirection={"column"}>
+          <Flex>
+            <Box mr={5}>
+              <NavLink
+                to="/dashboard/search"
+                activeStyle={{
+                  fontWeight: "bold",
+                  color: "#48BB78",
+                }}
+              >
+                Search Recipes
+              </NavLink>
+            </Box>
+            <Box mr={5}>Cook List</Box>
+            <Box>
+              <NavLink
+                to="/dashboard/create"
+                activeStyle={{
+                  fontWeight: "bold",
+                  color: "#48BB78",
+                }}
+              >
+                Create your recipe
+              </NavLink>
+            </Box>
+          </Flex>
+        </Flex>
+      </Flex>
+      <Box
+        width={"full"}
+        p={1}
+        border={2}
+        borderBottom={"1px"}
+        opacity={".5"}
+        mt={1}
+      ></Box>
+    </Box>
+  );
+};
+
+export default DashboardHeader;
