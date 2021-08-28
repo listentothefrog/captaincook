d75be9b932cadbb0dc63283944030153cd3798b5
listentothefrog <shashanke7y@gmail.com>
2021-08-27 21:59:28 -0400

    displays loading text with page route :)

--- triumph/src/components/Spinner.tsx
+++ triumph/src/components/Spinner.tsx
@@ -1,16 +1,28 @@
-import { Center } from "@chakra-ui/react";
+import { Center, Flex, Heading } from "@chakra-ui/react";
 import { Spinner } from "@chakra-ui/spinner";
+import { indexOf } from "lodash";
+import { useLocation } from "react-router";
 
 const SpinnerComponent = () => {
+  const location = useLocation();
   return (
     <Center>
-      <Spinner
-        thickness="3px"
-        speed="0.65s"
-        emptyColor="gray.200"
-        color="#48BB78"
-        size="md"
-      />
+      <Flex
+        width={"250px"}
+        height={"150px"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        flexDirection={"column"}
+      >
+        <Spinner
+          thickness="3px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="#48BB78"
+          size="xl"
+        />
+        <Heading size={"16px"}>Loading {location.pathname} </Heading>
+      </Flex>
     </Center>
   );
 };
