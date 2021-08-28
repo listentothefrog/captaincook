04d0e8e37337e4bc81a6d8b44faa2815683216e9
listentothefrog <shashanke7y@gmail.com>
2021-08-27 21:47:52 -0400

    removed apperance component import

--- triumph/src/pages/settings/settings.tsx
+++ triumph/src/pages/settings/settings.tsx
@@ -2,9 +2,6 @@ import { Box, Flex } from "@chakra-ui/react";
 import React, { Suspense } from "react";
 import SpinnerComponent from "src/components/Spinner";
 const Account = React.lazy(() => import("src/components/Settings/Account"));
-const Appearance = React.lazy(
-  () => import("src/components/Settings/Appearance")
-);
 
 const settings = () => {
   return (
