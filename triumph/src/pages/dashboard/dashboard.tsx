4e4b86b024af694f0919ea1e05cb122c7ab1bf55
listentothefrog <shashanke7y@gmail.com>
2021-08-31 17:39:27 -0400

    mobile dashboard header

--- triumph/src/pages/dashboard/dashboard.tsx
+++ triumph/src/pages/dashboard/dashboard.tsx
@@ -3,10 +3,12 @@ import React, { Suspense } from "react";
 import SpinnerComponent from "src/components/Spinner";
 const HeaderComponent = React.lazy(() => import("src/components/Header"));
 const DashboardHeader = React.lazy(
-  () => import("src/components/DashBoard/DashboardHeader")
+  () => import("src/components/DashBoard/DashboardHeader/DashboardHeader")
 );
 const FeedComponent = React.lazy(() => import("src/components/DashBoard/Feed"));
-
+const MobileDashboardComponent = React.lazy(
+  () => import("src/components/DashBoard/DashboardHeader/MobileDashboard")
+);
 const Dashboard = () => {
   return (
     <Box>
@@ -18,8 +20,37 @@ const Dashboard = () => {
         }
       >
         <HeaderComponent />
-        <DashboardHeader />
+        <Box
+          display={{ base: "none", lg: "flex" }}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Suspense
+            fallback={
+              <SpinnerComponent
+                message={"Crunching on this file, just for you. Hang tight..."}
+              />
+            }
+          >
+            <DashboardHeader />
+          </Suspense>
+        </Box>
         <FeedComponent />
+        <Box
+          width={{ base: "100%", lg: "0%" }}
+          height={{ base: "70vh", lg: "0vh" }}
+          display={{ base: "flex", lg: "none" }}
+        >
+          <Suspense
+            fallback={
+              <SpinnerComponent
+                message={"Crunching on this file, just for you. Hang tight..."}
+              />
+            }
+          >
+            <MobileDashboardComponent />
+          </Suspense>
+        </Box>
       </Suspense>
     </Box>
   );
