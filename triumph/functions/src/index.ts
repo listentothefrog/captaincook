d9ec1b1167c22dcd9bc98f16079156a37b7d2a40
listentothefrog <shashanke7y@gmail.com>
2021-09-05 20:11:50 -0400

    sends an welcome email (not deployed)

--- triumph/functions/src/index.ts
+++ triumph/functions/src/index.ts
@@ -1,9 +1,30 @@
 import * as functions from "firebase-functions";
+require("dotenv").config();
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+exports.sendWelcomeEmail = functions.auth.user().onCreate((_) => {
+  const email = "hellocaptiancook@gmail.com";
+  const nodemailer = require("nodemailer");
 
-export const helloWorld = functions.https.onRequest((_, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: email,
+      pass: process.env.EMAIL_PASSWORD_FOR_NODE_MAILER,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: "hihaha7608@mnqlm.com",
+    subject: "Welcome to CaptianCook",
+    text: "Hello",
+  };
+
+  transporter.sendMail(mailOptions, function (error: string, info: any) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 });
