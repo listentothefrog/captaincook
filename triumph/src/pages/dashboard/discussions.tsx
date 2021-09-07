263dfa24ad9012cf7f9495794b4d8fb1e34554a8
listentothefrog <shashanke7y@gmail.com>
2021-09-07 18:09:55 -0400

    reddit and captiancook collab

---
+++ triumph/src/pages/dashboard/discussions.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/layout";
+import HeaderComponent from "src/components/Header";
+
+const discussions = () => {
+  return (
+    <Box>
+      <HeaderComponent />
+      Discussions Tab
+    </Box>
+  );
+};
+
+export default discussions;
