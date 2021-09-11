cf6f81cbe495d14be64a753c8c0ead9940d93df4
listentothefrog <shashanke7y@gmail.com>
2021-09-11 14:33:37 -0400

    lazy load search component

--- triumph/src/pages/dashboard/search.tsx
+++ triumph/src/pages/dashboard/search.tsx
@@ -1,11 +1,21 @@
+import { Suspense } from "react";
 import SearchComponent from "src/components/DashBoard/Search";
 import HeaderComponent from "src/components/Header";
+import SpinnerComponent from "src/components/Spinner";
 
 const search = () => {
   return (
     <div>
       <HeaderComponent />
-      <SearchComponent />
+      <Suspense
+        fallback={
+          <SpinnerComponent
+            message={"Crunching on this file, just for you. Hang tight..."}
+          />
+        }
+      >
+        <SearchComponent />
+      </Suspense>
     </div>
   );
 };
