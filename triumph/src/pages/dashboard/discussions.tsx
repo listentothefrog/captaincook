2a06dec786529ef56a4193d3f11ec038e15b6084
listentothefrog <shashanke7y@gmail.com>
2021-09-12 18:39:31 -0400

    starter create a post component

--- triumph/src/pages/dashboard/discussions.tsx
+++ triumph/src/pages/dashboard/discussions.tsx
@@ -1,11 +1,35 @@
 import { Box } from "@chakra-ui/layout";
+import {
+  Flex,
+  FormControl,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import DashboardHeader from "src/components/DashBoard/DashboardHeader/DashboardHeader";
 import HeaderComponent from "src/components/Header";
+import search from "./search";
 
 const discussions = () => {
   return (
     <Box>
       <HeaderComponent />
-      Discussions Tab
+      <DashboardHeader />
+      <Box>
+        <Flex flexDir="row" mb="2" justifyContent="center" alignItems="center">
+          <Box width={"490px"}>
+            <Flex spacing={4} p="1rem">
+              <FormControl id="Post" isRequired>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none" />
+                  <Input type="string" placeholder="Create a Post" />
+                </InputGroup>
+              </FormControl>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
     </Box>
   );
 };
