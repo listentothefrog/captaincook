a57321243dcf0788c3c68597063f651c885f8b7c
listentothefrog <shashanke7y@gmail.com>
2021-09-12 20:05:21 -0400

    submit forum component

---
+++ triumph/src/components/DashBoard/SubmitForm.tsx
@@ -0,0 +1,16 @@
+import { Flex } from "@chakra-ui/react";
+
+const SubmitForm = () => {
+  return (
+    <Flex
+      width={"100%"}
+      height={"50vh"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      Hey :)
+    </Flex>
+  );
+};
+
+export default SubmitForm;
