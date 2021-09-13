afbb4e2a965f1adbd9ccc2476c019de39df9dadc
listentothefrog <shashanke7y@gmail.com>
2021-09-12 20:05:32 -0400

    render forum component

--- triumph/src/pages/dashboard/submit.tsx
+++ triumph/src/pages/dashboard/submit.tsx
@@ -1,7 +1,26 @@
 import { Box } from "@chakra-ui/react";
+import React, { Suspense } from "react";
+import HeaderComponent from "src/components/Header";
+import SpinnerComponent from "src/components/Spinner";
+const SubmitForm = React.lazy(
+  () => import("src/components/DashBoard/SubmitForm")
+);
 
 const submit = () => {
-  return <Box>Hello!</Box>;
+  return (
+    <Box>
+      <HeaderComponent />
+      <Suspense
+        fallback={
+          <SpinnerComponent
+            message={"Crunching on this file, just for you. Hang tight..."}
+          />
+        }
+      >
+        <SubmitForm />
+      </Suspense>
+    </Box>
+  );
 };
 
 export default submit;
