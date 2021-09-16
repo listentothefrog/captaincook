eaa948097ba35ee1d894e69caec16fc23d07e2bd
listentothefrog <shashanke7y@gmail.com>
2021-09-16 12:44:13 -0400

    export firestore variable

--- triumph/src/firebase/firebase.ts
+++ triumph/src/firebase/firebase.ts
@@ -1,11 +1,14 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/analytics";
+import "firebase/firestore";
 import { firebaseConfig } from "./firebaseConfig";
 
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
+
+export const firestore = firebase.firestore();
 
 firebase.analytics();
 
