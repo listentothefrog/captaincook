2fb5a6cc56dd0c362f8c381785065c8981361824
listentothefrog <shashanke7y@gmail.com>
2021-09-23 18:17:37 -0400

    improved posts ui

--- triumph/src/components/DashBoard/Posts.tsx
+++ triumph/src/components/DashBoard/Posts.tsx
@@ -1,51 +1,61 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
-import { BiUpvote, BiDownvote } from "react-icons/bi";
+import { Box, Flex, Text } from "@chakra-ui/layout";
 import { Post } from "src/interfaces/post";
+import { BsHeart } from "react-icons/bs";
+import { IoGitCompareSharp } from "react-icons/io5";
 
 const Posts = (props: Post) => {
-  const { textValue, upVotes, author, createdAt } = props;
+  const { textValue, author, likes } = props;
   return (
-    <Flex
-      mr={5}
-      ml={5}
-      mt={4}
-      p={5}
+    <Box
+      display={"flex"}
+      height={"150px"}
+      flexDirection={"column"}
+      m={4}
+      justifyContent={"center"}
       rounded={"lg"}
-      shadow="lg"
-      borderWidth="1px"
+      shadow={"lg"}
+      alignItems={"center"}
     >
-      <Box mr={2}>
-        <Box display={"flex"} flexDir={"column"}>
-          <Box cursor={"pointer"}>
-            <BiUpvote />
-          </Box>
-          <Box
-            display={"flex"}
-            alignContent={"center"}
-            justifyContent={"center"}
-          >
-            {upVotes}
-          </Box>
-          <Box cursor={"pointer"}>
-            <BiDownvote />
-          </Box>
+      <Box width={{ base: "80%", lg: "90%" }} height={"100%"} display={"flex"}>
+        <Box display={"flex"} width={"100%"} alignItems={"center"} flex={1}>
+          <Flex flexDir={"column"}>
+            <Flex mt={2} ml={4}>
+              <Text
+                fontFamily={"heading"}
+                fontSize={{ base: "xs", lg: "medium" }}
+              >
+                {textValue}
+              </Text>
+            </Flex>
+          </Flex>
+        </Box>
+        <Box>
+          <Text fontSize="12px" color="gray.400" mt={2} mr={2}>
+            Created by @{author}
+          </Text>
         </Box>
       </Box>
-      <Box flex={1}>
-        <Flex flexDir={"column"}>
-          <Flex mt={2} ml={4}>
-            <Text fontFamily={"body"} fontSize={"lg"}>
-              {textValue}
-            </Text>
-          </Flex>
-        </Flex>
+      <Box
+        display={"flex"}
+        width={"100%"}
+        alignItems={"center"}
+        justifyContent={"space-evenly"}
+        m={2}
+      >
+        <Box display={"flex"} alignItems={"center"}>
+          <Box mr={2} cursor={"pointer"}>
+            <BsHeart />
+          </Box>
+          {likes}
+        </Box>
+        <Box display={"flex"} alignItems={"center"}>
+          <Box mr={2} cursor={"pointer"}>
+            <IoGitCompareSharp />
+          </Box>
+          {likes}
+        </Box>
       </Box>
-      <Box>
-        <Text fontSize="12px" color="gray.400">
-          Created at {createdAt} by {author}
-        </Text>
-      </Box>
-    </Flex>
+    </Box>
   );
 };
 
