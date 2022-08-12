003c63da08e3484cefac1f4fe1eba8757a78c124
listentothefrog <shashanke7y@gmail.com>
2022-08-12 09:37:24 +0530

    log commits component

--- triumph/src/components/ChangeLogs.tsx
+++ triumph/src/components/ChangeLogs.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { Box, Flex, Heading, Link, Text, Divider } from "@chakra-ui/layout";
 
 const ChangeLogs = () => {
-  return <div>t3sting</div>;
+  return (
+    <Flex
+      mr={5}
+      ml={5}
+      p={5}
+      rounded={"lg"}
+      borderWidth="1px"
+      border={"transparent"}
+    >
+      <Box flex={1}>
+        <Flex flexDir={"column"} alignContent={"center"}>
+          <Flex alignItems={"center"} justifyContent="space-between">
+            <Link>
+              <Heading ml={"5"} fontSize="xl">
+                Commit message mm/dd/year
+              </Heading>
+            </Link>
+            <Link>
+              <Flex mt={2}>
+                <Text color={"#707070"}>by @username</Text>
+              </Flex>
+            </Link>
+          </Flex>
+          <Flex alignItems="flex-start" flexDir={"row"}>
+            <Box flex={1} mt={4} ml="5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+              laboriosam deserunt aspernatur nesciunt rerum consectetur eveniet
+              omnis, iure fuga exercitationem mollitia quasi blanditiis in
+              architecto eligendi illum eaque. Doloremque, soluta.
+            </Box>
+          </Flex>
+        </Flex>
+        <Divider
+          mt={"2"}
+          border={"1px"}
+          borderColor={"blackAlpha.600"}
+          ml={"5"}
+          width={"748"}
+        />
+      </Box>
+    </Flex>
+  );
 };
 
 export default ChangeLogs;
